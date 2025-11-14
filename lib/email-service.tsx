4af0683b5a5292ca8834d160dd12d551d6f4e5e6
Soneksss <sonek@gmail.com>
2025-11-14 20:50:20 +0000

    Update Resend to use non-null assertion for env variable

--- lib/email-service.tsx
+++ lib/email-service.tsx
@@ -1,7 +1,7 @@
 import { createClient } from "@/lib/server"
 import { Resend } from "resend"
 
-const resend = new Resend(process.env.RESEND_API_KEY)
+const resend = new Resend(process.env.RESEND_API_KEY!)
 
 export interface EmailTemplate {
   subject: string
